1754a90c65bc9985be0a4acbca04f93390e35207
Brad <bgreen3@uoregon.edu>
2016-10-23 19:20:38 -0600

    adds functionality for viewing jobs of companies by location

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,10 @@ class Company < ActiveRecord::Base
   validates :name, uniqueness: true
   has_many :jobs
   has_many :contacts
+
+  #sorts all companies by location
+  def self.sort_by_city
+    order(:city)
+  end
+
 end
